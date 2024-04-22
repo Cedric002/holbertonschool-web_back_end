15d56b9c597aa279f4b32d83a9e134597d757b7a
Cedric002 <cedgnleblanc@gmail.com>
2024-04-22 10:35:22 +0200

    add 1-main and 1-concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"Sum two String numbers with an type-annotation"
+
+
+def concat(str1: str, str2: str) -> str:
+    "Take two String numbers and returns the concatenated String"
+
+    return str1 + str2
