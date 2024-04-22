9e2a233f3db9027ff5d8c1a23ca36894b93165e5
Cedric002 <cedgnleblanc@gmail.com>
2024-04-22 15:12:51 +0200

    fix  2 5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
+from typing import List
+
 "Sum type float with an type-annotation sum_list wich take a list input_list"
 
 
 def sum_list(input_list: list[float]) -> float:
     "Take type sum_list and returns the Sum type float"
 
-    total = 0.0
-    for num in input_list:
-        total += num
-    return total
+    return sum(input_list)
