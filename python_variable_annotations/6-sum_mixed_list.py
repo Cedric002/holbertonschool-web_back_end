6d899ea93d1c2d4c7c063121fe6edd5f03329fa8
Cedric002 <cedgnleblanc@gmail.com>
2024-04-22 14:42:05 +0200

    pycostyle

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,7 +2,10 @@
 
 from typing import List, Union
 
-"Sum type float with an type-annotation sum_mixed_list wich take a list mxd_lst"
+"""
+Sum type float with an type-annotation sum_mixed_list 
+wich take a list mxd_lst
+"""
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
