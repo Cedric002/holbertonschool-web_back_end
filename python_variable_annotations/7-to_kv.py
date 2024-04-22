345346552e6b3f94ddb183efcc82e3246b8203f6
Cedric002 <cedgnleblanc@gmail.com>
2024-04-22 14:15:35 +0200

    add 7-main and 7-to_kv

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+"""
+Take a String k with an type-annotation to_kv and a int or Float v wich
+return a tuple.
+
+k is the first element of tuple
+v is a square of int or Float and is annoatated as a Float
+"""
+
+def to_kv(k: str, v: int | float) -> tuple[str, float]:
+    "Takes a string k and an int or Float v and returns a tuple"
+
+    return k, float(v ** 2)
