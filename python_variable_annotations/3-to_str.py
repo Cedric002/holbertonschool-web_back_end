eede684364c366d7012c099865b0d22e2ea636ee
Cedric002 <cedgnleblanc@gmail.com>
2024-04-22 11:01:13 +0200

    add 3-main and 3-to_str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"Take a Float n with an type-annotation to_str"
+
+
+def to_str(n: float) -> str:
+    "Take a Float n and returns the String representation of the float"
+
+    return str(n)
