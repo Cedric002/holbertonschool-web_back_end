6229a202a9ff78503f90ddd9cf43855139889faf
Cedric002 <cedgnleblanc@gmail.com>
2024-04-26 09:23:29 +0200

    fix 3 7-to_kv

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from typing import Tuple
+from typing import Tuple, Union
 
 """
 Take a string k with an type-annotation to_kv and a int or float v wich
@@ -11,7 +11,7 @@ v is a square of int or float and is annoatated as a float
 """
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     "Takes a string k and an int or float v and returns a tuple"
 
-    return k, float(v ** 2)
+    return k, v ** 2
