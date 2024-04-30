9dd90264440156a0d3c22ef4366f6b5e80f0a32f
Cedric002 <cedgnleblanc@gmail.com>
2024-04-30 10:15:28 +0200

    add 9-insert_school.py

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -5,8 +5,11 @@
 
 def insert_school(mongo_collection, **kwargs):
     """
-    Inserts the new document based on kwargs and
-    returns the new document's _id
+    Inserts the new document based on kwargs and returns
+    _id of the new document 
+
+    **kwargs is Keyword arguments representing the fields and
+    values of the new document
     """
     new_document = kwargs
     result = mongo_collection.insert_one(new_document)
