839c07b9c7990acd8650fb6c227b93f20bef1a82
Cedric002 <cedgnleblanc@gmail.com>
2024-04-30 11:08:27 +0200

    fix 11-schools_by_topic.py

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from pymongo import MongoClient
+
 "Returns the list of school that have a specific topic"
 
 
@@ -10,8 +12,11 @@ def schools_by_topic(mongo_collection, topic):
     mongo_collection is the MongoDB collection object
     topic is the topic to search for
     """
-    schools = list(mongo_collection.find({"topics": topic}, {"name": 1}))
+    cursor = mongo_collection.find({"topic": topic})
 
-    school_names = [school["name"] for school in schools]
+    schools = []
 
-    return school_names
+    for document in cursor:
+        schools.append(document['name'])
+
+    return schools
