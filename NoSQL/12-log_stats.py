394f0498783620bf1efe07cf44ed1444b3d6fdb7
Cedric002 <cedgnleblanc@gmail.com>
2024-05-03 10:40:59 +0200

    fix 2  12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,36 +5,39 @@
 from pymongo import MongoClient
 import pymongo
 
-# Connect to MongoDB
-client = MongoClient('mongodb://localhost:27017/')
-db = client['logs']  # Assuming MongoDB is running locally on default port
+if __name__ == "__main__":
 
-# Query the nginx collection
-collection = db['nginx']
+    # Connect to MongoDB
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client['logs']  # Assuming MongoDB is running locally on default port
 
-# Total number of documents in the collection
-total_docs = collection.count_documents({})
-print(f"{total_docs} logs")
+    # Query the nginx collection
+    collection = db['nginx']
 
-# Count of documents for each HTTP method
-methods_counts = {
-    "GET": 0,
-    "POST": 0,
-    "PUT": 0,
-    "PATCH": 0,
-    "DELETE": 0
-}
+    # Total number of documents in the collection
+    total_docs = collection.count_documents({})
+    print(f"{total_docs} logs")
 
-# Counting documents for each method
-for doc in collection.find():
-    method = doc.get('method', '')
-    if method in methods_counts:
-        methods_counts[method] += 1
+    # Count of documents for each HTTP method
+    methods_counts = {
+        "GET": 0,
+        "POST": 0,
+        "PUT": 0,
+        "PATCH": 0,
+        "DELETE": 0
+    }
 
-# Displaying the counts
-for method, count in methods_counts.items():
-    print(f"\t{count} {method}")
+    # Counting documents for each method
+    for doc in collection.find():
+        method = doc.get('method', '')
+        if method in methods_counts:
+            methods_counts[method] += 1
 
-# Count of documents with method=GET and path=/status
-get_status_count = collection.count_documents({"method": "GET", "path": "/status"})
-print(f"\t{get_status_count} GET /status")
+    # Displaying the counts
+    for method, count in methods_counts.items():
+        print(f"\t{count} {method}")
+
+    # Count of documents with method=GET and path=/status
+    get_status_count = collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"\t{get_status_count} GET /status")
