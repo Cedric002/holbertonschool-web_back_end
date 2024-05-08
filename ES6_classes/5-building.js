dad8afadce7389f8ec73cd86f12c3e6e47d4b5e5
Cedric002 <cedgnleblanc@gmail.com>
2024-05-08 17:00:22 +0200

    fix 5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -13,16 +13,3 @@ class Building {
     );
   }
 }
-
-const b = new Building(100);
-console.log(b);
-
-class TestBuilding extends Building {
-  // No evacuationWarningMessage method implemented
-}
-
-try {
-  new TestBuilding(200);
-} catch (err) {
-  console.log(err);
-}
