32101fdfb8258a4d9f4c4090549cb217d0b9d042
Cedric002 <cedgnleblanc@gmail.com>
2024-05-16 10:17:10 +0200

    add 6-main.js 6-final-user.js

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,24 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  try {
+    const [userResult, photoResult] = await Promise.allSettled([
+      userPromise,
+      photoPromise,
+    ]);
+
+    return [
+      { status: userResult.status, value: userResult.value },
+      { status: photoResult.status, value: photoResult.value },
+    ];
+  } catch (error) {
+    console.error("Error handling profile signup:", error.message);
+    return [];
+  }
+}
+
+export default handleProfileSignup;
