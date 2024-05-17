85c2c7365eacc55cd029b734feb2f65407b35905
Cedric002 <cedgnleblanc@gmail.com>
2024-05-17 11:13:19 +0200

    fix 2  9-try.js

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,10 +1,10 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-
   try {
-    queue.push(mathFunction());
-  } catch (err) {
-    queue.push(err.toString());
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
   } finally {
     queue.push("Guardrail was processed");
   }
