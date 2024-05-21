85ad9340805e96f7e5a9acd6ea64da07ea56b085
Cedric002 <cedgnleblanc@gmail.com>
2024-05-21 11:09:59 +0200

    add 12main.js and 2-get_students_byl_loc.js

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,8 @@
+const getStudentsByLocation = (students, city) => {
+  const studentsInCity = students.filter(
+    (student) => student.location === city
+  );
+  return studentsInCity;
+};
+
+export default getStudentsByLocation;
