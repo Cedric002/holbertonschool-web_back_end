0ac3a55b7cbc04d0b1650ade18d194205bdbf445
Cedric002 <cedgnleblanc@gmail.com>
2024-05-22 09:59:35 +0200

    fix 10-update_uniq_items.js

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 const updateUniqueItems = (map) => {
   if (!(map instanceof Map)) {
-    throw new Error("Cannot process");
+    throw new Error('Cannot process');
   }
 
   const updatedMap = new Map(map);
